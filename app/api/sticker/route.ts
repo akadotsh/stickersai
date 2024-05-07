effe140cff041da84c9fa0b76c3a5aaf3ce80471
0xakash9 <devakash256@gmail.com>
2024-05-07 08:26:13 +0530

    chore: remove edge

--- app/api/sticker/route.ts
+++ app/api/sticker/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import Replicate from "replicate";
 
-export const runtime = "edge";
-
 export async function POST(req: NextRequest) {
   const { prompt } = await req.json();
 
