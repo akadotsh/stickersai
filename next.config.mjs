9b4480e93cd5f62c2dd3ef3e95821a93a51022a1
0xakash9 <devakash256@gmail.com>
2024-05-07 08:24:49 +0530

    fix: node:crypto issue

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.externals.push({
+      "node:crypto": "commonjs crypto",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
